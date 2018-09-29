a029d2b79891091d8dda5ea1cf423121fa8b2b6e
morpheous <cerbus.collin@gmail.com>
2018-09-29 13:17:46 -0700

    4

--- FireFoxLauncher.js
+++ FireFoxLauncher.js
@@ -1,5 +1,6 @@
 const path = require( "path" );
-const ffpath = path.join( process.env.PWD ,"FireFoxLauncher.sh" );
+//const ffpath = path.join( process.env.PWD ,"FireFoxLauncher.sh" );
+const ffpath = path.join( __dirname , "FireFoxLauncher.sh" );
 const wSpawn = require("child_process").spawn;
 const cp = wSpawn( ffpath );
 cp.unref();
